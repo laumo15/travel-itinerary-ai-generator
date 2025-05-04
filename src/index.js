c8c4932d704cf7d42d01d327be8fe064df4f49be
laumo15 <118842475+laumo15@users.noreply.github.com>
2025-05-05 00:11:00 +0200

    UX improved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function displayItinerary(response) {
     console.log("generated");
+    console.log(response.data.answer)
 
     new Typewriter("#itinerary", {
     strings: response.data.answer,
@@ -16,8 +17,13 @@ function generateItinerary(event) {
 
     let apiKey = "t7ae374d4862800ofd885f7fba24b093";
     let prompt = `Generate a 5-day travel itinerary with ${destinationInput.value} as destination. Please do not include anything in your answer except the itinerary itself`
-    let context = "You are a travel expert and love to write travel itineraries. Your mission is to generate 5-day itineraries in basic HTML";
+    let context =
+      "You are a travel expert and love to write travel itineraries. Your mission is to generate 5-day itineraries in basic HTML, but please avoid using quotes and the word html at the beginning or other markdown"
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    let itineraryElement = document.querySelector("#itinerary")
+    itineraryElement.classList.remove("hidden")
+    itineraryElement.innerHTML = `<div class="blink">⌛ Generating your itinerary for ${destinationInput.value}...</div>`
 
     console.log("Generating itinerary");
     console.log(`Prompt: ${prompt}`)
