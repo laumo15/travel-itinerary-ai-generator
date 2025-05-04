cd573c27fb81503bd001cb637a73f31cbc099058
laumo15 <118842475+laumo15@users.noreply.github.com>
2025-05-04 23:43:37 +0200

    AI API integrated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-function generateItinerary(event) {
-    event.preventDefault()
+function displayItinerary(response) {
+    console.log("generated");
+
     new Typewriter("#itinerary", {
-      strings: ["Itinerary"],
-        autoStart: true,
-        delay: 1,
-      cursor: ""
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
     })
+}
+
+function generateItinerary(event) {
+    event.preventDefault();
+
+    let destinationInput = document.querySelector("#user-destination");
+
+    let apiKey = "t7ae374d4862800ofd885f7fba24b093";
+    let prompt = `Generate a 5-day travel itinerary with ${destinationInput.value} as destination. Please do not include anything in your answer except the itinerary itself`
+    let context = "You are a travel expert and love to write travel itineraries. Your mission is to generate 5-day itineraries in basic HTML";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+    console.log("Generating itinerary");
+    console.log(`Prompt: ${prompt}`)
+    console.log(`Context: ${context}`)
+
+    axios.get(apiUrl).then(displayItinerary);
 }
 
 let itineraryFormElement = document.querySelector("#itinerary-form")
